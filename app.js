9e4b0637302c3b71257f4e58c5839c792dc6dbb5
huzaifa26 <abidhuzaifa128@gmail.com>
2021-12-04 21:17:22 +0500

    make even it better

--- app.js
+++ app.js
@@ -214,5 +214,5 @@ app.post("/orderitems",(req,res)=>{
 let port=process.env.PORT || 3000
 
 app.listen(port,()=>{
-    console.log("Listening on 3000")
+    console.log("Listening on " + port)
 });
