ba42402708d0bfc7838a995c278b7a742d38c93c
huzaifa26 <abidhuzaifa128@gmail.com>
2021-12-25 11:20:55 +0500

    make it better

--- app.js
+++ app.js
@@ -370,7 +370,11 @@ app.post("/order",(req,res)=>{
         } 
 
         con.query("select count from product where id=?",req.body.cart[i].product_id,(err,result,fields)=>{
-          console.log(result)
+          console.log(result[0].count)
+          con.query("update product set count=? where id=?",[counter,req.body.cart[i].product_id],(err,result,fields)=>{
+            console.log("---------------------------------------------------")
+            console.log(result)
+          })
         })
 
 
